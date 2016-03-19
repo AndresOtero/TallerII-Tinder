4322cd1d32bb4e6da2d5ccbc5a29fca96053a42c
AndresOtero <oteroandres95@gmail.com>
2016-03-19 20:42:25 -0300

    Agrego un logger #1

--- TinderTallerII/src/main.cpp
+++ TinderTallerII/src/main.cpp
@@ -1,33 +1,58 @@
+
 #include "../lib/mongoose.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "../lib/easylogging++.h"
+
+INITIALIZE_EASYLOGGINGPP
+
+
 /** Implementacion de server web**/
 
 static const char *s_http_port = "8000";
 static struct mg_serve_http_opts s_http_server_opts;
 
+int logg()
+{
+	   LOG(INFO) << "My first info log using default logger";
+
+    return 0;
+}
 static void ev_handler(struct mg_connection *nc, int ev, void *p) {
   if (ev == MG_EV_HTTP_REQUEST) {
     mg_serve_http(nc, (struct http_message *) p, s_http_server_opts);
   }
 }
+void basic_server(){
+	struct mg_mgr mgr;
+	  struct mg_connection *nc;
 
-int main() {
-  struct mg_mgr mgr;
-  struct mg_connection *nc;
+	  mg_mgr_init(&mgr, NULL);
+	  nc = mg_bind(&mgr, s_http_port, ev_handler);
 
-  mg_mgr_init(&mgr, NULL);
-  nc = mg_bind(&mgr, s_http_port, ev_handler);
+	  // Set up HTTP server parameters
+	  mg_set_protocol_http_websocket(nc);
+	  s_http_server_opts.document_root = ".";  // Serve current directory
+	  s_http_server_opts.dav_document_root = ".";  // Allow access via WebDav
+	  s_http_server_opts.enable_directory_listing = "yes";
 
-  // Set up HTTP server parameters
-  mg_set_protocol_http_websocket(nc);
-  s_http_server_opts.document_root = ".";  // Serve current directory
-  s_http_server_opts.dav_document_root = ".";  // Allow access via WebDav
-  s_http_server_opts.enable_directory_listing = "yes";
+	  printf("Starting web server on port %s\n", s_http_port);
+	  for (;;) {
+	    mg_mgr_poll(&mgr, 1000);
+	  }
+	  mg_mgr_free(&mgr);
 
-  printf("Starting web server on port %s\n", s_http_port);
-  for (;;) {
-    mg_mgr_poll(&mgr, 1000);
-  }
-  mg_mgr_free(&mgr);
 
-  return 0;
+}
+
+int main(int argc,char*  argv[]) {
+	// Initialize Google's logging library.
+
+	logg();
+	basic_server();
+
+    return 0;
 }
