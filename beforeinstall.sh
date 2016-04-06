2bca7c89a2453dd76b21a9432eedce682581204a
AndresOtero <oteroandres95@gmail.com>
2016-04-06 13:26:56 -0300

    Cambio la version minima del cmake y rezo

--- beforeinstall.sh
+++ beforeinstall.sh
@@ -5,12 +5,6 @@ sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 #sudo apt-cache search libgflags-dev
 #sudo apt-get install libgflags-dev
 sudo apt-get install build-essential
-wget http://www.cmake.org/files/v3.5/cmake-3.5.0.tar.gz
-tar xf cmake-3.5.0.tar.gz
-cd cmake-3.5.0
-./configure
-make
-cd ..
 sudo apt-get install libsnappy-dev
 sudo apt-get install zlib1g-dev
 sudo apt-get install libbz2-dev
