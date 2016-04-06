ae014862e5de5f92f16e9169c56dea530b52b765
AndresOtero <oteroandres95@gmail.com>
2016-04-06 09:26:49 -0300

    Intento arreglar un nuevo c++11

--- beforeinstall.sh
+++ beforeinstall.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -ex
 sudo apt-get update
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-cache search libgflags-dev
 #sudo apt-get install libgflags-dev
 sudo apt-get install libsnappy-dev
 sudo apt-get install zlib1g-dev
 sudo apt-get install libbz2-dev
 sudo apt-get install libpthread-stubs0-dev
+sudo apt-get install -qq g++-4.8
