b970f0072ed699b0116cb16d3e23e58da022865f
AndresOtero <oteroandres95@gmail.com>
2016-04-06 09:14:32 -0300

    Intento arreglar una vez mas el libflags

--- beforeinstall.sh
+++ beforeinstall.sh
@@ -2,7 +2,7 @@
 set -ex
 sudo apt-get update
 sudo apt-cache search libgflags-dev
-sudo apt-get install libgflags-dev
+#sudo apt-get install libgflags-dev
 sudo apt-get install libsnappy-dev
 sudo apt-get install zlib1g-dev
 sudo apt-get install libbz2-dev
