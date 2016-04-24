afc542dcb606ccff5d296b938eb4fe0f864262cc
elianadiaz <diazeliana09@gmail.com>
2016-04-24 19:22:43 -0300

    Se saca el cliente del main

--- TinderTaller/src/main.cpp
+++ TinderTaller/src/main.cpp
@@ -10,7 +10,7 @@ INITIALIZE_EASYLOGGINGPP
 #include <cstdio>
 #include <string>
 #include <assert.h>
-#include "../include/Client.h"
+
 #include "../include/Server.h"
 #include "../include/DataBase.h"
 
@@ -18,7 +18,7 @@ int main(int argc, char* argv[]) {
 	shared_ptr<DataBase> db(new DataBase("./DBServer/", true, false));
 	shared_ptr<Server> server = Server::getServer();
 	server->setServerDB(db);
-	std::shared_ptr<Client> client(new Client());
+
 	bool continuar = true;
 	while (continuar && server->isSet()) {
 		server->runServer();
