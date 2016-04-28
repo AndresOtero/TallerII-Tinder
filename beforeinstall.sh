2eaaec1e7c208ed01246cb620af3c7b35be2641f
elianadiaz <diazeliana09@gmail.com>
2016-04-28 15:00:33 -0300

    Se agrega configuracion para Curl y un cliente para el ApplServer.

--- beforeinstall.sh
+++ beforeinstall.sh
@@ -9,3 +9,4 @@ sudo apt-get install libsnappy-dev
 sudo apt-get install zlib1g-dev
 sudo apt-get install libbz2-dev
 sudo apt-get install libpthread-stubs0-dev
+sudo apt-get install libcurl4-gnutls-dev
